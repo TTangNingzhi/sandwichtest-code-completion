6bb734b5a0488c393a65acea68150c1b94284241
Ningzhi Tang <ntang@jls00.cse.nd.edu>
2025-07-21 20:54:14 -0400

    Update project structure: added Environment.md for setup instructions, modified prepare_data.sh for correct data path handling, removed obsolete data files, and introduced evaluation notebook and environment configuration.

---
+++ Environment.md
@@ -0,0 +1,5 @@
+```
+conda create -n ast25-jb-completion python=3.11
+
+pip install jsonlines argparse tqdm rank-bm25 faiss-cpu transformers torch datasketch codegen-metrics vllm ipykernel
+```
--- prepare_data.sh
+++ prepare_data.sh
@@ -3,8 +3,8 @@
 STAGE=$1
 LANGUAGE=$2
 
-unzip data/$LANGUAGE-$STAGE -d data/repositories-$LANGUAGE-$STAGE
+unzip ../data/$LANGUAGE-$STAGE -d ../data/repositories-$LANGUAGE-$STAGE
 
-for zipfile in data/repositories-$LANGUAGE-$STAGE/*.zip; do
+for zipfile in ../data/repositories-$LANGUAGE-$STAGE/*.zip; do
   unzip -o "$zipfile" -d "${zipfile%.zip}"
 done
